89c0984b7461cec02aa2ccd2ff7f94328c743b21
NavinReigns <116470053+NavinReigns@users.noreply.github.com>
2022-11-21 05:37:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/antonyponnavin/EvaMaria /EvaMaria
+  git clone https://github.com/NavinReigns/Jvn_Advanced_Filter_Bot /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
